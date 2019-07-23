d59b62877fe42cb6028026e087876c13fb96b1ce
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-23 21:15:18 +1000

    Define Timestamped<Value> wrapper to decorate Value with timestamp

--- Sources/Timestamped/Timestamped.swift
+++ Sources/Timestamped/Timestamped.swift
@@ -1,3 +1,21 @@
-struct Timestamped {
-    var text = "Hello, World!"
+import Foundation
+
+/// A wrapper to decorate a type with timestamp information.
+public struct Timestamped<Value> {
+
+  public let value: Value
+  public let created: Date
+
+  public init(value: Value, created: Date = Date()) {
+    self.value = value
+    self.created = created
+  }
+
+  public func createdLater(than date: Date) -> Bool {
+    return created.timeIntervalSince(date) > 0
+  }
+
+  public func createdBefore(than date: Date) -> Bool {
+    return created.timeIntervalSince(date) < 0
+  }
 }
--- Tests/TimestampedTests/TimestampedTests.swift
+++ Tests/TimestampedTests/TimestampedTests.swift
@@ -1,15 +1,27 @@
 import XCTest
-@testable import Timestamped
+import Timestamped
 
 final class TimestampedTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Timestamped().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testCreatedLater() {
+    let referenceDate = Date()
+    let timestamped = Timestamped(value: "abc", created: referenceDate)
+
+    XCTAssertTrue(timestamped.createdLater(than: Date.distantPast))
+    XCTAssertTrue(timestamped.createdLater(than: referenceDate.addingTimeInterval(-1)))
+    XCTAssertFalse(timestamped.createdLater(than: referenceDate))
+    XCTAssertFalse(timestamped.createdLater(than: referenceDate.addingTimeInterval(1)))
+    XCTAssertFalse(timestamped.createdLater(than: Date.distantFuture))
+  }
+
+  func testCreatedBefore() {
+    let referenceDate = Date()
+    let timestamped = Timestamped(value: "abc", created: referenceDate)
+
+    XCTAssertFalse(timestamped.createdBefore(than: Date.distantPast))
+    XCTAssertFalse(timestamped.createdBefore(than: referenceDate.addingTimeInterval(-1)))
+    XCTAssertFalse(timestamped.createdBefore(than: referenceDate))
+    XCTAssertTrue(timestamped.createdBefore(than: referenceDate.addingTimeInterval(1)))
+    XCTAssertTrue(timestamped.createdBefore(than: Date.distantFuture))
+  }
 }
