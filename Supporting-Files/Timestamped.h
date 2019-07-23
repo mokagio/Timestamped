f71ab05dc759843ce2ce692d40984203dbcefec8
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-07-23 21:01:49 +1000

    Create skeleton project for framework
    Used https://github.com/tonyarnold/Differ as a reference, thanks
    @tonyarnold ;)

---
+++ Supporting-Files/Timestamped.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for Timestamped.
+FOUNDATION_EXPORT double TimestampedVersionNumber;
+
+//! Project version string for Timestamped.
+FOUNDATION_EXPORT const unsigned char TimestampedVersionString[];
